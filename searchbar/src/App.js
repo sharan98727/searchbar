06ffa913ff206b5fdd8650ca8674b2dceefacfab
reddi sharan <reddisharan@gmail.com>
2020-08-17 18:02:28 +0530

    changes added

--- searchbar/src/App.js
+++ searchbar/src/App.js
@@ -1,44 +1,72 @@
 import React from 'react';
+import Searchresults from './searchresults';
+
+let data = [
+    {name:'india'},
+    {name:'srilanka'},
+    {name:'usa'},
+    {name:'russia'},
+    {name:'spain'},
+]
 
 class App extends React.Component{
 
     state = {
         input:'',
+        database:data,
     }
 
     handlechange = e =>{
         this.setState({
             input:e.target.value,
         })
+        
     }
 
     makecall = () =>{
-          console.log('entered');
+          //console.log('entered');
+           const suggestions = this.state.database.filter(country =>{
+               
+               if(country.name.startsWith(this.state.input))
+               {
+                   
+                   return country.name;
+               }
+            //    else
+            //    {
+            //         return 'no related searches'
+            //    }
+          })
+         console.log(suggestions);
     }
 
     debounce = (fn,delay) =>{
 
         let callapi;
-        
+       return function(){ 
         clearTimeout(callapi);
         callapi = setTimeout(()=>{
-          fn();
-         
+         fn();
+          
         },delay);
-        
+    } 
     }
-
+    
+    betterfunction = this.debounce(this.makecall,2000)
 
     render(){
         return(
+    <div>
         <form >
             <input placeholder='search anything'
              value={this.state.input} 
              onChange={this.handlechange}
-             onKeyUp={()=>{this.debounce(this.makecall,2000)}}
-             style={{margin:'200px 300px 300px 300px'}}
+             onKeyUp={this.betterfunction}
+             style={{margin:'50px 300px 0px 500px'}}
              />
-        </form>     
+        </form>
+        <Searchresults item={this.state.input}/>
+    </div>     
         )
     }
 
